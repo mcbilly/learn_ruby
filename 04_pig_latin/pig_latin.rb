ee34ab1396e92db212d9a1b47ed1b36dfdcb2f4b
McBilly <mcbillywilford@gmail.com>
2016-07-31 00:35:46 -0400

    Added student solutions

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,35 @@
 #write your code here
+def translate(string)
+	input = string
+	sentence_output = ""
+	array_of_words = string.split(" ")
+	
+	array_of_words.each do |word|
+
+		array_of_char = word.split""
+		runOnce = false
+		consonants_before_vowel = ""
+		word_output = ""
+
+		array_of_char.each_with_index do |char, index|
+			if char == 'a' || char == 'e' || char == 'i' || char == "o" || (char == "u" && word[index-1] != 'q')
+				if index == 0
+					runOnce = true
+				elsif runOnce == false
+				consonants_before_vowel = word[0...index]
+				runOnce = true
+				end
+			end
+			word_output = word_output + char
+		end
+		
+		if consonants_before_vowel != ""
+				word_output = word_output[consonants_before_vowel.length,word_output.length]
+		end
+		
+		word_output = word_output + consonants_before_vowel + "ay"
+		sentence_output = sentence_output + word_output + " "
+	end
+
+	return sentence_output.strip
+end
